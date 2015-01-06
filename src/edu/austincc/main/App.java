471f9b0febfc4df1db88469cb34bad1fe665107d
Jake Dromgoole <tjd210@yahoo.com>
2015-01-06 11:22:00 -0600

    Added source file in a package for the main App

---
+++ src/edu/austincc/main/App.java
@@ -0,0 +1,22 @@
+
+
+
+package edu.austincc.main;
+
+
+/**
+ * @author javauser
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hellow, world");
+	}
+
+}
